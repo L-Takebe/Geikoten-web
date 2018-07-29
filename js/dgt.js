5b7ee239d00c9a5851ca2c023c5a89d11a614a99
L-Takebe <33525582+L-Takebe@users.noreply.github.com>
2018-07-29 13:30:20 +0900

    Add files via upload

---
+++ js/dgt.js
@@ -0,0 +1,32 @@
+// JavaScript Document
+
+jQuery(function() {
+    var nav = jQuery('#for_pc_nav');
+	var sp = jQuery('#spaces');
+    var offsetTop = nav.offset().top;
+
+    var floatMenu = function(){
+        if (jQuery(window).scrollTop() > offsetTop - 10) {
+            nav.addClass('kotei_pc');
+			sp.addClass('space');
+        } else {
+            nav.removeClass('kotei_pc');
+			sp.removeClass('space');
+        }
+    }
+    jQuery(window).scroll(floatMenu);
+    jQuery('body').bind('touchmove', floatMenu);
+});
+
+	$(function(){
+   $("#mob_menu").css("display","none");
+   $("#for_mob_menu").on("click", function() {
+      $("#mob_menu").slideToggle(300);
+      $(this).toggleClass("active");
+   });
+});
+	$(function(){
+    $("#mob_menu").on("click", function() {
+      $(this).css("display","none");
+   });
+});
